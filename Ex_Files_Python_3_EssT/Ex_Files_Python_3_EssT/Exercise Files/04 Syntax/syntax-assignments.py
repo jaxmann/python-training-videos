e0d84c253c618adfcf8fb4d7b90826550efa157e
Jonathan Axmann <jonathan.axmann09@gmail.com>
2017-02-13 17:16:32 -0500

    chapter 4 progress

---
+++ Ex_Files_Python_3_EssT/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntax-assignments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# syntax.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    a = [1,2,3,4,5]
+    print(a)
+
+
+if __name__ == "__main__": main()
---
+++ Ex_Files_Python_3_EssT/Ex_Files_Python_3_EssT/Exercise Files/04 Syntax/syntax-objects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# syntax.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+class Egg:
+    def __init__(self, kind = "fried"):
+        self.kind = kind
+
+    def whatKind(self):
+        return self.kind
+
+def main():
+    e = Egg("boiled")
+    print(e.whatKind())
+
+if __name__ == "__main__": main()
