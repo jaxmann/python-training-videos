f2f2b7a12070912658f610b1a3153ff1c05161e2
Jonathan Axmann <jonathan.axmann09@gmail.com>
2017-02-14 11:33:05 -0500

    chapter 7 progress

---
+++ Ex_Files_Python_3_EssT/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-working2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    v = 'one'
+    if v == 'one':
+        print('v is one')
+    elif v == 'two':
+        print('v is two')
+    else:
+        print('v is.')
+
+if __name__ == "__main__": main()
