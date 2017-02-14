1e1b79a16ca96b7f8cb5bc2e6816f312a41aaf7a
Jonathan Axmann <jonathan.axmann09@gmail.com>
2017-02-14 11:33:05 -0500

    chapter 6 progress

---
+++ Ex_Files_Python_3_EssT/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-working.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    a, b = 0, 1
+    if False:
+        print('this is true')
+    else:
+        print('it is not true')
+
+if __name__ == "__main__": main()
---
+++ Ex_Files_Python_3_EssT/Ex_Files_Python_3_EssT/Exercise Files/06 Conditionals/conditionals-working3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# conditionals.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+def main():
+    a, b = 0, 1
+    v = 'this is true' if a < b else 'this is not true'
+    print(v)
+
+if __name__ == "__main__": main()
